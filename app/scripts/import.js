3c2964f2197edf5fae1b98f22145ff091dcf82a8
Teodor Proca <procateodor@Bytexs-MacBook-Pro-2.local>
2022-01-27 15:12:48 +0200

    change db

--- app/scripts/import.js
+++ app/scripts/import.js
@@ -24,32 +24,59 @@ const RISKS = {
   disclosure: 2,
   overflow: 1,
   buffer: 1,
-  metasploit: 3
+  metasploit: 3,
 };
 
-const TOTAL_RISKS_SCORE = Object.values(RISKS).reduce((sum, value) => sum + value, 0);
+const TOTAL_RISKS_SCORE = Object.values(RISKS).reduce(
+  (sum, value) => sum + value,
+  0
+);
 
-const getScaledValue = (value, sourceRangeMin, sourceRangeMax, targetRangeMin, targetRangeMax) => {
+const getScaledValue = (
+  value,
+  sourceRangeMin,
+  sourceRangeMax,
+  targetRangeMin,
+  targetRangeMax
+) => {
   const targetRange = targetRangeMax - targetRangeMin;
   const sourceRange = sourceRangeMax - sourceRangeMin;
-  return (value - sourceRangeMin) * targetRange / sourceRange + targetRangeMin;
+  return (
+    ((value - sourceRangeMin) * targetRange) / sourceRange + targetRangeMin
+  );
 };
 
 const getRiskLevel = (vulnerability) => {
-  let risk = Object.keys(RISKS).reduce((score, key) => vulnerability.description[1].toLowerCase().includes(key) ? score + RISKS[key] : score, 0);
+  let risk = Object.keys(RISKS).reduce(
+    (score, key) =>
+      vulnerability.description[1].toLowerCase().includes(key)
+        ? score + RISKS[key]
+        : score,
+    0
+  );
   const value = Math.ceil(getScaledValue(risk, 0, TOTAL_RISKS_SCORE / 5, 1, 5));
   return value > 5 ? 5 : value;
 };
 
-// lightOrm.driver = mysql.createConnection(`mysql://${process.env.USERNAME}:${process.env.PASSWORD}@${process.env.HOST}/${process.env.DATABASE}?ssl=true`);
+// lightOrm.driver = mysql.createConnection(
+//   `mysql://${process.env.USERNAME}:${process.env.PASSWORD}@${process.env.HOST}/${process.env.DATABASE}?reconnect=true`
+// );
 // lightOrm.driver.connect();
-//
+
 // lightOrm.driver.query("delete from vulnerabilities where 1;", () => {
 //   for (const vul of data) {
-//     lightOrm.driver.query(`insert into vulnerabilities (id, description, type, platform, date_published, verified, port, author, risk) values ('${vul.id}', '${vul.description[1]}', '${vul.type_id}', '${vul.platform_id}', '${vul.date_published}', '${vul.verified}', '${vul.port}', '${vul.author_id[1]}', '${Math.ceil(getRiskLevel(vul))}')`);
+//     lightOrm.driver.query(
+//       `insert into vulnerabilities (id, description, type, platform, date_published, verified, port, author, risk) values ('${
+//         vul.id
+//       }', '${vul.description[1]}', '${vul.type_id}', '${vul.platform_id}', '${
+//         vul.date_published
+//       }', '${vul.verified}', '${vul.port}', '${vul.author_id[1]}', '${Math.ceil(
+//         getRiskLevel(vul)
+//       )}')`
+//     );
 //   }
 // });
 
 module.exports = {
-  getRiskLevel
-}
+  getRiskLevel,
+};
